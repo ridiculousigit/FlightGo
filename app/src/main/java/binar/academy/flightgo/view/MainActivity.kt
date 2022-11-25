3c1ad8db59a6d7352560b2abe9d7dc076abb9f08
ridiculousigit <jsigit243@gmail.com>
2022-11-25 17:05:46 +0700

    Initial Commit

--- app/src/main/java/binar/academy/flightgo/view/MainActivity.kt
+++ app/src/main/java/binar/academy/flightgo/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package binar.academy.flightgo
+package binar.academy.flightgo.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import binar.academy.flightgo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
