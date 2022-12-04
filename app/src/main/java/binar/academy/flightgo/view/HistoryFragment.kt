a3c98150a1e33d30914d24dc10070638fb65668a
ibal.libul <ibal.libul>
2022-12-04 23:27:31 +0700

    comite UI Disen news

---
+++ app/src/main/java/binar/academy/flightgo/view/HistoryFragment.kt
@@ -0,0 +1,22 @@
+package binar.academy.flightgo.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import binar.academy.flightgo.R
+
+
+class HistoryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_history, container, false)
+    }
+
+
+}
